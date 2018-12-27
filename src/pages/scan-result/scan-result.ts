0bc4566a1542ad3bda4c64b7389e59c03421bc6b
Story5 <337995545@qq.com>
2018-12-27 15:42:37 +0800

    platform

--- src/pages/scan-result/scan-result.ts
+++ src/pages/scan-result/scan-result.ts
@@ -20,9 +20,6 @@ export class ScanResultPage {
     console.log("ionViewDidLoad format:",this.format);
     this.text = this.navParams.get('text');
     console.log("ionViewDidLoad text:", this.text);
-
-    this.format = "CODE_128";
-    this.text = "341003930455561";
   }
 
 }
